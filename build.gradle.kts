e68db0815370978f51da424c85e323a158eee9dc
Eaquel <shaleofangel@gmail.com>
2026-05-19 21:39:28 +0300

    Fix target 24 limitation: inject Kotlin 2.3.21 strictly into root classpath to natively support JVM 25 toolchain like Backrooms project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
 plugins {
     id("com.android.application") version "9.2.0" apply false
+    // Kotlin 2.3.21'i (Java 25 destekleyen sürüm) projeye zorla dayatıyoruz!
+    id("org.jetbrains.kotlin.android") version "2.3.21" apply false
 }
